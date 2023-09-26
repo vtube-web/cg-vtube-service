e600b780776f475f4bc761ff8d67a2a2a6025d18
leduyphong123 <leduyphongck789@gmail.com>
2023-09-26 11:18:04 +0700

    project init

---
+++ src/main/java/com/example/cgvtubeservice/CgVtubeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgvtubeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgVtubeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgVtubeServiceApplication.class, args);
+	}
+
+}
